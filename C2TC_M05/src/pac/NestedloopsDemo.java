8912feabccd4a3f37ed8bfb9f94a9d06aa2774e4
harsh <harsh@DESKTOP-K8PBM74>
2022-01-24 18:47:33 +0530

    first commit

---
+++ C2TC_M05/src/pac/NestedloopsDemo.java
@@ -0,0 +1,17 @@
+package pac;
+
+public class NestedloopsDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			for(int j=1;j<=10;j++)
+			{
+				System.out.println("Value of i:"+i+"and Value of j:"+j);
+			}
+			System.out.println("****************************");
+		}
+
+	}
+
+}
